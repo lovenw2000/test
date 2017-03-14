610b835bd622a4b7f41bd71ee258a57f69d8c96c
lovenw2000 <jz_generator@126.com>
2017-03-14 19:40:31 +0800

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>HTML</title>
+		<style type="text/css">
+			
+		</style>
+	</head>
+	<body>
+    <h1>The HTML test file</h1>
+  
+    <script type="text/javascript">
+			
+			
+		</script>
+	</body>
+</html>
